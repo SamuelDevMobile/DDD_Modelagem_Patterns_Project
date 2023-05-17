0d53ea9c830a809e26edcf4dc795121966349860
Samuel Ribeiro <samuelribeiro9@icloud.com>
2023-05-17 01:18:33 -0300

    finalizacao de entidades

--- fc-ddd-patterns/src/entity/customer.ts
+++ fc-ddd-patterns/src/entity/customer.ts
@@ -1,23 +1,48 @@
+
+// Complexidade de negocio
+// Domain
+// - Entity
+//   - - customer.ts (regra de negócio)
+
+// Complexidade acidental
+// Infra - Mundo externo
+// - Entity / Model
+//   - - customer.ts (get,set)
+
 class Customer {
     
     _id: string;
     _name: string;
-    _address: string;
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
     }
 
-    get id() { return this._id; }
+    validate() {
+        if (this._name.length === 0) { 
+            throw new Error("Name is required")
+        }
+        if (this._id.length === 0) { 
+            throw new Error("Id is required")
+        }
+    }
 
-    get name() { return this._name; }
-    
-    get address() { return this._address }
+    changeName(name: string) {
+        this._name = name
+        this.validate();
+    }
 
-    set name(name: string) { this._name = name; }
+    activate() { 
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer")
+        }
+        this._active = true;
+     }
 
-    set address(address: string) { this._address = address; }
-    
+    desactivate() { this._active = false; }
 }
+ let custome = new Customer("123", "");
